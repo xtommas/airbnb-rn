6a3372162b5a504b328b87fe730239b70672fcc1
tomas <tomascanepa1397@gmail.com>
2024-02-23 17:02:12 -0300

    initial commit

--- app/(tabs)/inbox.tsx
+++ app/(tabs)/inbox.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const Page = () => {
+const Inbox = () => {
   return (
     <View>
-      <Text>Page</Text>
+      <Text>Inbox</Text>
     </View>
   );
 };
 
-export default Page;
+export default Inbox;
